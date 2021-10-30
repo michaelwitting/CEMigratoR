8cb58c67cb81ae52e4e558de1e06e71ffb094c19
michaelwitting <michael.witting@helmholtz-muenchen.de>
2021-10-30 13:56:28 +0200

    initial function ideas

---
+++ R/convert.R
@@ -0,0 +1,25 @@
+#'
+#'
+#'
+convertMigrationTime <- function(x, marker, method = c("single", "multiple")) {
+
+  # files in x and number of files in marker have to be of same length
+
+
+  # if single
+
+
+  # else if mulitple
+}
+
+#'
+#'
+convertSingle <- function() {
+
+}
+
+#'
+#'
+convertMultiple <- function() {
+
+}
---
+++ R/find.R
@@ -0,0 +1,17 @@
+#'
+#'
+#'
+findMigrationMarker <- function(x, marker) {
+
+  # for each file in x
+    # for each marker in DF
+
+}
+
+#'
+#'private function
+.findMM <- function(x, singleMarker) {
+
+
+  # return a single numeric = migration time of marker
+}
